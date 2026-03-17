48dc5724b6ceb91ca4c4d990f5da598db08b93ee
Nikul <nikulthakor6458@gmail.com>
2026-03-17 22:36:22 +0530

    solved bt vercel

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,6 @@
 import { getLoggedInUser } from "@/lib/auth";
 import { connectDB } from "@/lib/connectDB";
-
+// he dependencies solved by the server
 export async function GET() {
   await connectDB();
   const user = await getLoggedInUser();
